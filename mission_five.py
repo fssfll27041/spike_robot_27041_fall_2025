6a5325334afa2618114fb8511d56364979725f1e
FSS FLL 27041 <fss.fll.27041@gmail.com>
2025-10-09 09:39:23 -0400

    MIssion Five Update

--- mission_five.py
+++ mission_five.py
@@ -4,7 +4,7 @@
 # Description:
 # [Describe What your mission does here]
 #
-# Author(s): [Your Name(s)]
+# Author(s): Josie Evelyn
 # Date: [YYYY-MM-DD]
 # Version: 1.0
 #
@@ -19,6 +19,18 @@ from pybricks.tools import wait, StopWatch
 def mission_five(r):
     print("Running Mission 5")
     # Your code goes here...
+    r.robot.straight(775)
+    r.robot.turn(90)
+    r.robot.straight(400)
+    r.robot.turn(-100)
+    r.ram.run_time(1000,500)
+    r.robot.straight(200)
+    
+    
+
+
+
+
 
 ################################
 # KEEP THIS AT THE END OF THE FILE
