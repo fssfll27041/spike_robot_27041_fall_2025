eacc115cb1fd8f9cf149d3b62a96e06ebf6fbd55
FSS FLL 27041 <fss.fll.27041@gmail.com>
2025-10-31 13:52:06 -0400

    Update Mission 1 for arc (Win)

--- mission_one.py
+++ mission_one.py
@@ -32,57 +32,48 @@ TURN_ACCEL = 200  # deg/sec^2
 
 def mission_one(r):
     print("Running Mission 1")
-    # Your code goes here...
-    # Sample Code: Run attachment motors and drive motors
-    # r.lam.run_time(1000,1000)
-    # r.lam.run_time(-1000,1000)
-    # r.ldm.run_time(1000,1000)
-    # r.ldm.run_time(-1000,1000)
-    # r.rdm.run_time(400,1000)
-    # r.rdm.run_time(-400,1000)
 
-    # position wheel 6 units on jig
-    # move from home  to silo
-    r.robot.straight(425)     
-    # chop the lever
-    for i in range(4):
-        r.ram.run_time(1300, 600)
-        r.ram.run_time(-1300, 600)
+    # ------------------------------------------------------
+    # position wheel 6 units on jig before mission start
+    # ------------------------------------------------------
+
+    # move from home to silo
+    r.robot.straight(415)     
+    # chop the lever to eject frisbees
+     for i in range(4):
+        r.ram.run_time(1300, 697)
+        r.ram.run_time(-1300, 697)
 
     # start market stall mission
-
     # turn left and proceed 20-30 cm
-    r.robot.turn(-55)
+    r.robot.turn(-52)
     r.robot.straight(275)
 
     # slight turn to the left then push boom foward
     r.robot.turn(-55)
-    r.robot.straight(200)
+    r.robot.straight(260)
 
-    # Drive to the rear of tip the scales
-    r.robot.straight(280)
-    r.robot.turn(30)
-    r.robot.straight(170)
-    r.robot.turn(80)
-    r.robot.straight(320)
-    r.robot.turn(50)
-    r.robot.straight(60)
+    # Drive to the rear of tip the scales using arc turn
+    r.robot.arc(200 , 100)
+    r.robot.straight(20)
+    r.robot.turn(80) 
+    r.robot.straight(90)
+    r.robot.turn(40)    
+
+    # Wack raised basket
     r.ram.run_time(1300, 580)
     r.ram.run_time(-1300, 580)
 
     # Go back to home
-    r.robot.turn(-5)
+    r.robot.turn(-17)
     # enable turbo mode
     r.robot.settings(1000, 1000, TURN_RATE, TURN_ACCEL)
+    # race backwards to home
     r.robot.straight(-1000)
 
-    #disable turbo mode
+    # disable turbo mode before other missions
     r.robot.stop()
     r.robot.settings(STRAIGHT_SPEED, STRAIGHT_ACCEL, TURN_RATE, TURN_ACCEL)
-
-
-
-
 
 ################################
 # KEEP THIS AT THE END OF THE FILE
