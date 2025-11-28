cda25a13bf6e2c01a472f8ad604abcbc4aed6a4e
FSS FLL 27041 <fss.fll.27041@gmail.com>
2025-11-28 10:25:24 -0500

    Enhance Mission 6 (Mac)

--- mission_six.py
+++ mission_six.py
@@ -1,17 +1,13 @@
 ################################################################################
 # mission_six.py
-#
-# Description:
-# [Describe What your mission does here]
-#
-# Author(s): Josie Evelyn
-# Date: [2025-11-8]
+#Josie and Evelyn 
+# our mission does and s-turn up to the lifter then drives to the "what's for sale" and raises the roof.
+
 # Version: 1.0
 #
 # Dependencies:
 # - robot
 # - pybricks.tools
-#
 ################################################################################
 from robot import robot
 from pybricks.tools import wait, StopWatch
@@ -19,15 +15,19 @@ from pybricks.tools import wait, StopWatch
 def mission_six(r):
     print("Running Mission 6")
     # Your code goes here...
-
+#s-turn begins mission
     r.robot.settings(700, 300, 300, 200)
-    r.robot.straight(700)
-    r.robot.drive(1000, 90)
-    wait(900) 
-    r.robot.straight(200)
-    r.robot.settings(400, 300, 300, 200)
+    r.robot.straight(677)
+    r.robot.drive(1000,93)
+    wait(970)
+    #it drives up to minecart lifter and lifts, 30 points 
+    r.robot.straight(175)
+    r.ram.run_time(-200,1000)
+    #it raises the roof, 20 points
+    r.robot.straight(580)
+    r.robot.turn(45)
+    r.robot.straight(400)
 
-    #r.robot.turn(90)
     """#r.robot.straight(390)
     #r.robot.turn(-95)
     #r.ram.run_time(1000,500)
@@ -42,19 +42,21 @@ def mission_six(r):
     r.robot.straight(655)
     r.robot.turn(40)
     r.robot.straight(350)
-    r.robot.straight(-100)
-    r.robot.turn(-30)
-    r.robot.drive(1000,20)
-    wait(1000)
+    """
+    r.robot.straight(-300)
+    r.robot.turn(-45)
+    r.robot.drive(1000,41)
+    wait(500)
     r.robot.drive(1000,-20)
     wait(2000)
     r.robot.stop()    
     r.robot.settings(400, 300, 300, 200)
     # r.robot.straight(200)
     #r.robot.turn(30)
-    #r.robot.straight(900)
-    """
+    #r.robot.straight(900)   
+
    
+
 ################################
 # KEEP THIS AT THE END OF THE FILE
 # This redirects to running main.
