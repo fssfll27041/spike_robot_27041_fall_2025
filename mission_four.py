759a404e6e3ad54735041e4c93b1cacb8ddbc4fb
FSS FLL 27041 <fss.fll.27041@gmail.com>
2025-12-08 12:33:32 -0500

    Major changes to millstone mission 4

--- mission_four.py
+++ mission_four.py
@@ -2,7 +2,7 @@
 # mission_four.py
 #
 # Description:
-# Push bolders/ Collect millistone
+# Push bolders / Collect millistone
 # 
 # Authors: Eleanor Andreu Jack
 # Date: 2025-11-22
@@ -19,27 +19,44 @@ from pybricks.tools import wait, StopWatch
 def mission_four(r):
     print("Running Mission 4")
  
-    # gerald heavy lifting code:
+    # Revised millstone mission from qualifier
 
-    # Drive forward past the silo and push the ore out of bounds
-    r.robot.straight(695)
-    r.robot.turn(70)
-    r.robot.straight(350)
-    r.robot.drive(300,-110)
-    wait(870)
-    r.robot.stop()
-    # Drive to millstone and pick it up using the amazing comb
-   # r.robot.turn(-75)
-   # r.ram.run_time(200,2000)
-    r.robot.straight(40)
-    r.robot.turn(-37)
-    r.robot.straight(20)
+    r.robot.straight(95)
+    r.robot.turn(45)
+    r.robot.straight(220)
+    r.robot.turn(-45)
+    r.robot.straight(570)
+    r.robot.turn(-45)
+    r.ram.run_time(200,2000)
+    r.robot.straight(200)
     r.ram.run_time(-200,2000)
+    r.robot.straight(-200)
 
     # Return to home, arcing a bit to avoid the wall
-    r.robot.drive(-500,10)
-    wait(2000)
-    r.robot.stop()
+    # r.robot.drive(-500,10)
+    # wait(2000)
+    # r.robot.stop()
+
+    # Drive forward past the silo and push the ore out of bounds
+    # r.robot.straight(695)
+    # r.robot.turn(70)
+    # r.robot.straight(350)
+    # r.robot.drive(300,-110)
+    # wait(870)
+    # r.robot.stop()
+
+    # Drive to millstone and pick it up using the amazing comb
+    # r.robot.turn(-75)
+    # r.ram.run_time(200,2000)
+    # r.robot.straight(40)
+    # r.robot.turn(-37)
+    # r.robot.straight(20)
+    # r.ram.run_time(-200,2000)
+
+    # Return to home, arcing a bit to avoid the wall
+    # r.robot.drive(-500,10)
+    # wait(2000)
+    # r.robot.stop()
 
 if __name__ == "__main__":
     from main import main
